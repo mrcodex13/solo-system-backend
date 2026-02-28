0fd7c274f50b774ea2b4ab708531acb6af7861bf
mrcodex13 <kartiksingh2173@gmail.com>
2026-02-28 17:36:24 +0530

    port problrm fixed try

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/data', dataRoutes);
 app.get('/', (req, res) => res.json({ status: 'System Online' }));
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    const PORT = process.env.PORT || 5000;
+
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
       startScheduler();
